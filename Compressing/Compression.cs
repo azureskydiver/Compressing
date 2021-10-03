a16f52fcbb6334489262797eb5f8f5697c4d5731
Ants <code.herder@recombulator.net>
2021-10-02 22:27:02 -0400

    Encode bytes into UTF-16 text string
    Base64 is not space efficient. Base64 was intended to be a binary to
    text encoding that would allow delivery of the data across various
    mediums where the only commonly supported would be the most common
    7-bit alpanumeric characters. Since the Base64 string is being stored
    in a C#'s native string is composed of 16-bit characters encoded as
    UTF-16, 9-bits worth of potential storage space is wasted for every
    character used.
    
    Each pair of bytes from the original data is taken to form the low and
    high bytes of the new Unicode character that will be added to the
    string.
    
    If ever a pair will cause the character to fall within the surrogate
    range (U+D800-U+DFFF), the low and high bytes are encoded as surrogate
    pair of U+D800 + low, and U+DC00 + high.
    
    The CompressionValue record type has been updated to show the UTF-16
    string length. Previously it would store a Base64 string, but show the
    byte lengths of the unencoded bytes.

--- Compressing/Compression.cs
+++ Compressing/Compression.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
@@ -24,8 +26,8 @@ namespace Compressing
             var result = output.ToArray();
 
             return new CompressionResult(
-                new(value, bytes.Length),
-                new(Convert.ToBase64String(result), result.Length),
+                new(value),
+                new(result.ToPackedUtf16()),
                 level,
                 algorithm);
         }
@@ -38,7 +40,7 @@ namespace Compressing
 
         private static async Task<string> FromCompressedStringAsync(string value, Func<Stream, Stream> createDecompressionStream)
         {
-            var bytes = Convert.FromBase64String(value);
+            var bytes = value.PackedUtf16ToBytes().ToArray();
             await using var input = new MemoryStream(bytes);
             await using var output = new MemoryStream();
             await using var stream = createDecompressionStream(input);
@@ -54,6 +56,55 @@ namespace Compressing
  
         public static async Task<string> FromBrotliAsync(this string value)
             => await FromCompressedStringAsync(value, s => new BrotliStream(s, CompressionMode.Decompress));
+
+        private static bool IsSurrogate(byte b)
+            => 0xD8 <= b && b <= 0xDF;
+
+        public static string ToPackedUtf16(this byte[] data)
+        {
+            var pairs = data.Length / 2;
+            bool isOdd = data.Length % 2 == 1;
+            var sb = new StringBuilder(pairs + (isOdd ? 1 : 0));
+            var bytePairs = pairs * 2;
+
+            for (int i = 0; i < bytePairs; i += 2)
+                EncodeBytes(low: data[i], high: data[i + 1]);
+
+            if (isOdd)
+                EncodeBytes(low: data[data.Length - 1], high: 0x00);
+
+            return sb.ToString();
+
+            void EncodeBytes(byte low, byte high)
+            {
+                if (IsSurrogate(high))
+                {
+                    AppendChar(low: low, high: 0xD8);
+                    AppendChar(low: high, high: 0xDC);
+                }
+                else
+                {
+                    AppendChar(low: low, high: high);
+                }
+            }
+
+            void AppendChar(byte low, byte high)
+                => sb.Append((char)((high << 8) + low));
+        }
+
+        public static IEnumerable<byte> PackedUtf16ToBytes(this string packedUtf16)
+        {
+            foreach(char ch in packedUtf16)
+            {
+                byte low = (byte) (ch & 0xFF);
+                byte high = (byte)((ch >> 8) & 0xFF);
+
+                if (!IsSurrogate(high))
+                    yield return high;
+
+                yield return low;
+            }
+        }
     }
 
     public record CompressionResult(
@@ -71,7 +122,9 @@ namespace Compressing
     }
 
     public record CompressionValue(
-        string Value,
-        int Size
-    );
+        string Value
+    )
+    {
+        public int Size => Value.Length;
+    }
 }
